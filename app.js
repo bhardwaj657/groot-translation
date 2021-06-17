773adb44838e730f2da5e04bdfc1e74387092fc7
Shubham <shubham2002bharat@gmail.com>
2021-06-18 04:33:55 +0530

    groot-translation-app

---
+++ app.js
@@ -0,0 +1,23 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#input-txt");
+var outputDiv = document.querySelector("#output-div");
+
+var serverURL ="https://api.funtranslations.com/translate/groot.json";
+function getTranslatedURL(text){
+    return serverURL + "?" + "text=" + text
+}
+function errorHandler(error){
+    console.log("error",error)
+    alert("Ooops! something went wrong")
+}
+function clickHandler(){
+var inputText= txtInput.value;    
+fetch(getTranslatedURL(inputText))
+.then(response => response.json())
+.then(json =>{
+    var translatedText=json.contents.translated;
+    outputDiv.innerText= translatedText;
+})
+.catch(errorHandler)
+}
+btnTranslate.addEventListener("click",clickHandler);
